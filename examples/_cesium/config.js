b4d9abf34542b9da9e324d58e897e969136d9029
jiawanlong <trampjwl1@163.com>
2024-05-22 17:18:54 +0800

    提交

---
+++ examples/_cesium/config.js
@@ -0,0 +1,534 @@
+/* by jiawanlong*/
+var identification = {
+    name: "Leaflet"
+};
+var exampleConfig = {
+    "base": {
+        name: "1、基础",
+        name_en: "base",
+        content: {
+            "map": {
+                name: "1.1、基础",
+                name_en: "base",
+                content: [{
+                    name: "1.1、默认设置",
+                    name_en: "1.1、默认设置",
+                    thumbnail: "1.1、默认设置.png",
+                    fileName: "1.1、默认设置"
+                },
+                {
+                    name: "1.2、颜色",
+                    name_en: "1.2、颜色",
+                    thumbnail: "1.2、颜色.png",
+                    fileName: "1.2、颜色"
+                },
+                {
+                    name: "1.3、坐标、坐标转换",
+                    name_en: "1.3、坐标、坐标转换",
+                    thumbnail: "1.3、坐标、坐标转换.png",
+                    fileName: "1.3、坐标、坐标转换"
+                },
+                {
+                    name: "1.4、屏幕、场景、地表坐标",
+                    name_en: "1.4、屏幕、场景、地表坐标",
+                    thumbnail: "1.4、屏幕、场景、地表坐标.png",
+                    fileName: "1.4、屏幕、场景、地表坐标"
+                },
+                {
+                    name: "1.5、相机",
+                    name_en: "1.5、相机",
+                    thumbnail: "1.5、相机.png",
+                    fileName: "1.5、相机"
+                },
+                {
+                    name: "1.6、事件",
+                    name_en: "1.6、事件",
+                    thumbnail: "1.6、事件.png",
+                    fileName: "1.6、事件"
+                },
+                {
+                    name: "1.7、飞行漫游",
+                    name_en: "1.7、飞行漫游",
+                    thumbnail: "1.7、飞行漫游.png",
+                    fileName: "1.7、飞行漫游"
+                },
+                {
+                    name: "1.8、时间运动",
+                    name_en: "1.8、时间运动",
+                    thumbnail: "1.8、时间运动.png",
+                    fileName: "1.8、时间运动"
+                },
+                {
+                    name: "1.9、气泡窗口",
+                    name_en: "1.9、气泡窗口",
+                    thumbnail: "1.9、气泡窗口.png",
+                    fileName: "1.9、气泡窗口"
+                },
+                {
+                    name: "1.10、反选遮罩",
+                    name_en: "1.10、反选遮罩",
+                    thumbnail: "1.10、反选遮罩.png",
+                    fileName: "1.10、反选遮罩"
+                },
+                {
+                    name: "1.11、加载shp",
+                    name_en: "1.11、加载shp",
+                    thumbnail: "1.11、加载shp.png",
+                    fileName: "1.11、加载shp"
+                },
+                {
+                    name: "1.12、加载mvt",
+                    name_en: "1.12、加载mvt",
+                    thumbnail: "1.12、加载mvt.png",
+                    fileName: "1.12、加载mvt"
+                },
+                {
+                    name: "1.13、地表透明度",
+                    name_en: "1.13、地表透明度",
+                    thumbnail: "1.13、地表透明度.png",
+                    fileName: "1.13、地表透明度"
+                },
+                {
+                    name: "1.14、倾斜摄影单体",
+                    name_en: "1.14、倾斜摄影单体",
+                    thumbnail: "1.14、倾斜摄影单体.png",
+                    fileName: "1.14、倾斜摄影单体"
+                },
+                {
+                    name: "1.15、罗盘、导航条",
+                    name_en: "1.15、罗盘、导航条",
+                    thumbnail: "1.15、罗盘、导航条.png",
+                    fileName: "1.15、罗盘、导航条"
+                },
+                {
+                    name: "1.16、绕点飞行",
+                    name_en: "1.16、绕点飞行",
+                    thumbnail: "1.16、绕点飞行.png",
+                    fileName: "1.16、绕点飞行"
+                },
+                {
+                    name: "1.17、深度检测",
+                    name_en: "1.17、深度检测",
+                    thumbnail: "1.17、深度检测.png",
+                    fileName: "1.17、深度检测"
+                },
+                    // {
+                    //     name: "相机",
+                    //     name_en: "相机",
+                    //     thumbnail: "1.5、相机.png",
+                    //     fileName: "1.5、相机"
+                    // },
+                    // {
+                    //     name: "事件",
+                    //     name_en: "事件",
+                    //     thumbnail: "1.6、事件",
+                    //     fileName: "1.6、事件"
+                    // }
+
+
+                ]
+            },
+        }
+    },
+    "iPortal": {
+        name: "2、图层",
+        name_en: "layer",
+        content: {
+            "map1": {
+                name: "2.1、影像",
+                name_en: "2.1、影像",
+                content: [{
+                    name: "2.1.1、单张图片",
+                    name_en: "2.1.1、单张图片",
+                    thumbnail: "2.1.1、单张图片.png",
+                    fileName: "2.1.1、单张图片"
+                },
+                {
+                    name: "2.1.2、xyz服务",
+                    name_en: "2.1.2、xyz服务",
+                    thumbnail: "2.1.2、xyz服务.png",
+                    fileName: "2.1.2、xyz服务"
+                },
+                {
+                    name: "2.1.3、tms服务",
+                    name_en: "2.1.3、tms服务",
+                    thumbnail: "2.1.3、tms服务.png",
+                    fileName: "2.1.3、tms服务"
+                },
+                {
+                    name: "2.1.4、wms服务",
+                    name_en: "2.1.4、wms服务",
+                    thumbnail: "2.1.4、wms服务.png",
+                    fileName: "2.1.4、wms服务"
+                },
+                {
+                    name: "2.1.5、wmts服务",
+                    name_en: "2.1.5、wmts服务",
+                    thumbnail: "2.1.5、wmts服务.png",
+                    fileName: "2.1.5、wmts服务"
+                },
+                {
+                    name: "2.1.6、天地图服务",
+                    name_en: "2.1.6、天地图服务",
+                    thumbnail: "2.1.6、天地图服务.png",
+                    fileName: "2.1.6、天地图服务"
+                },
+                {
+                    name: "2.1.7、4490服务",
+                    name_en: "2.1.7、4490服务",
+                    thumbnail: "2.1.7、4490服务.png",
+                    fileName: "2.1.7、4490服务"
+                },
+                {
+                    name: "2.1.8、4490服务2",
+                    name_en: "2.1.8、4490服务2",
+                    thumbnail: "2.1.8、4490服务2.png",
+                    fileName: "2.1.8、4490服务2"
+                },
+                {
+                    name: "2.1.9、影像操作",
+                    name_en: "2.1.9、影像操作",
+                    thumbnail: "2.1.9、影像操作.png",
+                    fileName: "2.1.9、影像操作"
+                },
+                {
+                    name: "2.1.10、加载shp",
+                    name_en: "2.1.10、加载shp",
+                    thumbnail: "1.11、加载shp.png",
+                    fileName: "1.11、加载shp"
+                },
+                {
+                    name: "2.1.11、加载mvt",
+                    name_en: "2.1.11、加载mvt",
+                    thumbnail: "1.12、加载mvt.png",
+                    fileName: "1.12、加载mvt"
+                },
+                ]
+            },
+            "map2": {
+                name: "2.2、地形",
+                name_en: "2.2、地形",
+                content: [
+                    {
+                        name: "2.2.1、离线地形",
+                        name_en: "2.2.1、离线地形",
+                        thumbnail: "2.2.1、离线地形.png",
+                        fileName: "2.2.1、离线地形"
+                    },
+                    {
+                        name: "2.2.2、默认地形",
+                        name_en: "2.2.2、默认地形",
+                        thumbnail: "2.2.2、默认地形.png",
+                        fileName: "2.2.2、默认地形"
+                    },
+                    {
+                        name: "2.2.3、地形夸张",
+                        name_en: "2.2.3、地形夸张",
+                        thumbnail: "2.2.3、地形夸张.png",
+                        fileName: "2.2.3、地形夸张"
+                    },
+                ]
+            },
+            "map3": {
+                name: "2.3、矢量",
+                name_en: "2.3、矢量",
+                content: [
+                    {
+                        name: "2.3.1、entity点",
+                        name_en: "2.3.1、entity点",
+                        thumbnail: "2.3.1、entity点.png",
+                        fileName: "2.3.1、entity点"
+                    },
+                    {
+                        name: "2.3.2、entity线",
+                        name_en: "2.3.2、entity线",
+                        thumbnail: "2.3.2、entity线.png",
+                        fileName: "2.3.2、entity线"
+                    },
+                    {
+                        name: "2.3.3、entity面",
+                        name_en: "2.3.3、entity面",
+                        thumbnail: "2.3.3、entity面.png",
+                        fileName: "2.3.3、entity面"
+                    },
+                    {
+                        name: "2.3.4、图标",
+                        name_en: "2.3.4、图标",
+                        thumbnail: "2.3.4、图标.png",
+                        fileName: "2.3.4、图标"
+                    },
+                    {
+                        name: "2.3.5、海量图标",
+                        name_en: "2.3.5、海量图标",
+                        thumbnail: "2.3.5、海量图标.png",
+                        fileName: "2.3.5、海量图标"
+                    },
+                    {
+                        name: "2.3.6、popup",
+                        name_en: "2.3.6、popup",
+                        thumbnail: "2.3.6、popup.png",
+                        fileName: "2.3.6、popup"
+                    },
+                    {
+                        name: "2.3.7、geojson",
+                        name_en: "2.3.7、geojson",
+                        thumbnail: "2.3.7、geojson.png",
+                        fileName: "2.3.7、geojson"
+                    },
+                    {
+                        name: "2.3.8、Primitive",
+                        name_en: "2.3.8、Primitive",
+                        thumbnail: "2.3.8、Primitive.png",
+                        fileName: "2.3.8、Primitive"
+                    },
+                    {
+                        name: "2.3.9、AllPrimitive",
+                        name_en: "2.3.9、AllPrimitive",
+                        thumbnail: "2.3.9、AllPrimitive.png",
+                        fileName: "2.3.9、AllPrimitive"
+                    },
+                    {
+                        name: "2.3.10、entityCollection",
+                        name_en: "2.3.10、entityCollection",
+                        thumbnail: "2.3.10、entityCollection.png",
+                        fileName: "2.3.10、entityCollection"
+                    },
+                    {
+                        name: "2.3.11、entity操作",
+                        name_en: "2.3.11、entity操作",
+                        thumbnail: "2.3.11、entity操作.png",
+                        fileName: "2.3.11、entity操作"
+                    },
+                    {
+                        name: "2.3.12、气泡窗口",
+                        name_en: "2.3.12、气泡窗口",
+                        thumbnail: "1.9、气泡窗口.png",
+                        fileName: "1.9、气泡窗口"
+                    },
+                    {
+                        name: "2.3.13、气泡弹窗",
+                        name_en: "2.3.13、气泡弹窗",
+                        thumbnail: "2.3.13、气泡弹窗.png",
+                        fileName: "2.3.13、气泡弹窗"
+                    },
+                ]
+            },
+            "map4": {
+                name: "2.4、模型",
+                name_en: "2.4、模型",
+                content: [
+                    {
+                        name: "2.4.1、加载模型",
+                        name_en: "2.4.1、加载模型",
+                        thumbnail: "2.4.1、加载模型.png",
+                        fileName: "2.4.1、加载模型"
+                    },
+                    {
+                        name: "2.4.2、primitive加载模型",
+                        name_en: "2.4.2、primitive加载模型",
+                        thumbnail: "2.4.2、primitive加载模型.png",
+                        fileName: "2.4.2、primitive加载模型"
+                    },
+                    {
+                        name: "2.4.3、primitive加载大量模型",
+                        name_en: "2.4.3、primitive加载大量模型",
+                        thumbnail: "2.4.3、primitive加载大量模型.png",
+                        fileName: "2.4.3、primitive加载大量模型"
+                    },
+                    {
+                        name: "2.4.4、模型运动",
+                        name_en: "2.4.4、模型运动",
+                        thumbnail: "1.8、时间运动.png",
+                        fileName: "1.8、时间运动"
+                    },
+                    {
+                        name: "2.4.5、模型插值运动",
+                        name_en: "2.4.5、模型插值运动",
+                        thumbnail: "2.4.5、模型插值运动.png",
+                        fileName: "2.4.5、模型插值运动"
+                    },
+                ]
+            },
+        }
+    },
+    "Online": {
+        name: "3、3Dtiles",
+        name_en: "3、3Dtiles",
+        content: {
+            "map5": {
+                name: "3.1、3Dtiles",
+                name_en: "3.1、3Dtiles",
+                content: [
+                    {
+                        name: "3.1.1、加载模型",
+                        name_en: "3.1.1、加载模型",
+                        thumbnail: "3.1.1、加载模型.png",
+                        fileName: "3.1.1、加载模型"
+                    },
+                    {
+                        name: "3.1.2、模型偏移",
+                        name_en: "3.1.2、模型偏移",
+                        thumbnail: "3.1.2、模型偏移.png",
+                        fileName: "3.1.2、模型偏移"
+                    },
+                    {
+                        name: "3.1.3、模型位置编辑",
+                        name_en: "3.1.3、模型位置编辑",
+                        thumbnail: "3.1.3、模型位置编辑.png",
+                        fileName: "3.1.3、模型位置编辑"
+                    },
+                    {
+                        name: "3.1.4、customShader自定义材质",
+                        name_en: "3.1.4、customShader自定义材质",
+                        thumbnail: "3.1.4、customShader自定义材质.png",
+                        fileName: "3.1.4、customShader自定义材质"
+                    },
+                    {
+                        name: "3.1.5、customShader自定义材质2",
+                        name_en: "3.1.5、customShader自定义材质2",
+                        thumbnail: "3.1.5、customShader自定义材质2.png",
+                        fileName: "3.1.5、customShader自定义材质2"
+                    },
+                ]
+            },
+        }
+    },
+    "iManager": {
+        name: "4、空间分析",
+        name_en: "4、空间分析",
+        content: {
+            "map5": {
+                name: "4.1、空间分析",
+                name_en: "4.1、空间分析",
+                content: [
+                    {
+                        name: "4.1.1、地形开挖",
+                        name_en: "4.1.1、地形开挖",
+                        thumbnail: "4.1.1、地形开挖.png",
+                        fileName: "4.1.1、地形开挖"
+                    },
+                    {
+                        name: "4.1.2、可视域分析",
+                        name_en: "4.1.2、可视域分析",
+                        thumbnail: "4.1.2、可视域分析.png",
+                        fileName: "4.1.2、可视域分析"
+                    },
+                    {
+                        name: "4.1.3、淹没分析（简易）",
+                        name_en: "4.1.3、淹没分析（简易）",
+                        thumbnail: "4.1.3、淹没分析（简易）.png",
+                        fileName: "4.1.3、淹没分析（简易）"
+                    },
+                    {
+                        name: "4.1.4、淹没分析",
+                        name_en: "4.1.4、淹没分析",
+                        thumbnail: "4.1.4、淹没分析.png",
+                        fileName: "4.1.4、淹没分析"
+                    },
+                    {
+                        name: "4.1.5、缓冲分析",
+                        name_en: "4.1.5、缓冲分析",
+                        thumbnail: "4.1.5、缓冲分析.png",
+                        fileName: "4.1.5、缓冲分析"
+                    },
+                    {
+                        name: "4.1.6、日照分析",
+                        name_en: "4.1.6、日照分析",
+                        thumbnail: "4.1.6、日照分析.png",
+                        fileName: "4.1.6、日照分析"
+                    },
+                    {
+                        name: "4.1.7、方量分析",
+                        name_en: "4.1.7、方量分析",
+                        thumbnail: "4.1.7、方量分析.png",
+                        fileName: "4.1.7、方量分析"
+                    },
+                    {
+                        name: "4.1.8、透视分析",
+                        name_en: "4.1.8、透视分析",
+                        thumbnail: "4.1.8、透视分析.png",
+                        fileName: "4.1.8、透视分析"
+                    },
+                    {
+                        name: "4.1.9、剖面分析",
+                        name_en: "4.1.9、剖面分析",
+                        thumbnail: "4.1.9、剖面分析.png",
+                        fileName: "4.1.9、剖面分析"
+                    },
+                    
+                ]
+            },
+            "map6": {
+                name: "4.2、量算",
+                name_en: "4.2、量算",
+                content: [
+                    {
+                        name: "4.2.1、量算工具",
+                        name_en: "4.2.1、量算工具",
+                        thumbnail: "4.2.1、量算工具.png",
+                        fileName: "4.2.1、量算工具"
+                    },
+                    
+                ]
+            },
+            "map7": {
+                name: "4.3、视频融合",
+                name_en: "4.3、视频融合",
+                content: [
+                    {
+                        name: "4.3.1、视频纹理",
+                        name_en: "4.3.1、视频纹理",
+                        thumbnail: "4.3.1、视频纹理.png",
+                        fileName: "4.3.1、视频纹理"
+                    },
+                    
+                ]
+            },
+        }
+    },
+};
+
+
+/**
+ *key值：为exampleConfig配置的key值或者fileName值
+ *      （为中间节点时是key值，叶结点是fileName值）
+ *value值：fontawesome字体icon名
+ *不分层
+ */
+var sideBarIconConfig = {
+    "base": "fa-server",
+    "iPortal": "fa-desktop",
+    "Online": "fa-cloud",
+    "iManager": "fa-group",
+    "Elasticsearch": "fa-tasks",
+    "plot": "fa-edit",
+    "dynamicPlot": "fa-pencil",
+    "control": "fa-sliders",
+    "components": "fa-window-restore",
+    "clientSpatialAnalyst": "fa-object-group",
+    "viz": "fa-map",
+    "OGC": "fa-globe",
+    "mapping": "fa-send"
+};
+
+/**
+ *key值：为exampleConfig配置的key值
+ *value值：fontawesome字体icon名
+ *与sideBarIconConfig的区别：sideBarIconConfig包括侧边栏所有层级目录的图标，exampleIconConfig仅包括一级标题的图标
+ */
+var exampleIconConfig = {
+    "base": "fa-server",
+    "iPortal": "fa-desktop",
+    "Online": "fa-cloud",
+    "iManager": "fa-group",
+    "Elasticsearch": "fa-tasks",
+    "plot": "fa-edit",
+    "dynamicPlot": "fa-pencil",
+    "control": "fa-sliders",
+    "components": "fa-window-restore",
+    "clientSpatialAnalyst": "fa-object-group",
+    "viz": "fa-map",
+    "OGC": "fa-globe",
+    "mapping": "fa-send"
+};
+window.leafletExampleConfig = exampleConfig;
